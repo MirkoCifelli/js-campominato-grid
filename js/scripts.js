9749858a20732b4f56dab3b54a947764c1aed7b0
MirkoCifelli <ciffo4@hotmail.it>
2023-12-14 10:49:56 +0100

    Add bonus

--- js/scripts.js
+++ js/scripts.js
@@ -7,36 +7,62 @@
 
 
 
+// const play = document.querySelector("button")
+// console.log('play', play, typeof play)
+
+// let screen = false;
+// play.addEventListener("click", function(){
+//     screen.toogle;
+//     const quatrato = document.querySelector('main > .container');
+//     if(screen){
+//         quatrato.innerHTML='';
+//         screen = false
+//     }
+//     else{
+//         generate(quatrato);
+//         screen = true;
+//     }
+// })
+
+
+
+// function generate(qualcosa) {
+//     for (let i = 1; i <= 100; i++) {
+//         const cell = document.createElement('div');
+//         cell.classList.add('cell');
+//         cell.innerHTML = i;
+//         qualcosa.append(cell);
+//         cell.addEventListener("click", function(){
+//             console.log(this);
+//             this.classList.toggle('active');
+//             console.log(this.innerHTML)
+//         })
+//     }
+    
+// }
+
 const play = document.querySelector("button")
 console.log('play', play, typeof play)
 
-let screen = false;
-play.addEventListener("click", function(){
-    screen.toogle;
-    const quatrato = document.querySelector('main > .container');
-    if(screen){
-        quatrato.innerHTML='';
-        screen = false
-    }
-    else{
-        generate(quatrato);
-        screen = true;
-    }
-})
+const diff = document.getElementById('difficulty');
 
+const container = document.querySelector('main > .grid-container')
+    console.log('container', container, typeof container)
 
-
-function generate(qualcosa) {
-    for (let i = 1; i <= 100; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.innerHTML = i;
-        qualcosa.append(cell);
-        cell.addEventListener("click", function(){
-            console.log(this);
-            this.classList.toggle('active');
-            console.log(this.innerHTML)
-        })
-    }
+play.addEventListener('click', function(){
+    const cellNumber = parseInt(diff.value);
     
-}
+    container.innerHTML= '';
+
+    for (let i = 1; i <= cellNumber; i++){
+        const cell = document.createElement('div');
+        cell.innerHTML=  i ;
+        cell.classList.add('cell');
+        cell.classList.add('cell-' + cellNumber);
+        cell.addEventListener('click', function(){
+            this.classList.add('active');
+            console.log(this.innerHTML);
+        });
+        container.append(cell);
+     }
+})
